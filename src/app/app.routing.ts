c29ed180e49efa458f6dca9298405e9e800ea4e8
Christopher Sosa <urri_@hotmail.com>
2022-10-11 14:50:43 -0500

    Sistema de rutas registro

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,14 +2,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoute: Routes = [
   {
     path: '',
     component: InicioComponent,
-  }, {
+  },
+  {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'registro',
+    component: RegisterComponent,
   },
 ];
 
