182dbadbc1a444e2c2a548a6f399016169bb60c8
Christopher Sosa <urri_@hotmail.com>
2022-10-12 12:50:42 -0500

    Cambios en login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClienteService } from 'src/app/services/cliente.service';
+declare var iziToast: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public user: any = {};
+  public usuario: any = {};
+  public token;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _clienteService: ClienteService,
+    private _router: Router
+  ) {
+    this.token = localStorage.getItem('token');
+    if (this.token) {
+      this._router.navigate(['/']);
+    }
   }
 
+  ngOnInit(): void {}
+
+  login(loginForm: any) {
+    if (loginForm.valid) {
+      let data = {
+        email: this.user.email,
+        password: this.user.password,
+      };
+      this._clienteService.login_cliente(data).subscribe(
+        (res) => {
+          if (res.data == undefined) {
+            iziToast.show({
+              title: 'ERROR',
+              class: 'text-danger',
+              titleColor: '#ff0000',
+              position: 'topRight',
+              message: res.message,
+            });
+          } else {
+            this.usuario = res.data;
+            localStorage.setItem('token', res.token);
+            localStorage.setItem('_id', res.data._id);
+
+            this._router.navigate(['/']);
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      iziToast.show({
+        title: 'ERROR',
+        class: 'text-danger',
+        titleColor: '#ff0000',
+        position: 'topRight',
+        message: 'Los datos del formulario no son validos.',
+      });
+    }
+  }
 }
