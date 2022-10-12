cf97b754329e903ac166cd4dd39b76d4d3fd7ef0
Christopher Sosa <urri_@hotmail.com>
2022-10-12 12:50:59 -0500

    Cambios en nav para visualizar al usuario

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  public token;
+  public id;
+  public user: any = undefined;
+  public user_lc: any = undefined;
 
-  constructor() { }
+  constructor(private _clienteService: ClienteService) {
+    this.token = localStorage.getItem('token');
+    this.id = localStorage.getItem('_id');
 
-  ngOnInit(): void {
+    if (localStorage.getItem('user_data')) {
+      this.user_lc = JSON.parse(localStorage.getItem('user_data') || '');
+    } else {
+      this.user_lc = undefined;
+    }
+
+    this._clienteService.obtener_cliente_guest(this.id, this.token).subscribe(
+      (res) => {
+        this.user = res.data;
+        localStorage.setItem('user_data', JSON.stringify(this.user));
+      },
+      (err) => {
+        this.user = undefined;
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
