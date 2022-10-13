55070ba829130d024ccbe783eae2eb235e48a06e
Christopher Sosa <urri_@hotmail.com>
2022-10-13 10:28:36 -0500

    Inplementacion de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public user: any = {};
+  public usuario: any = {};
+  public token;
+  constructor(private _router: Router) {
+    this.token = localStorage.getItem('token');
+    if (this.token) {
+      this._router.navigate(['/']);
+    }
   }
 
+  ngOnInit(): void {}
 }
