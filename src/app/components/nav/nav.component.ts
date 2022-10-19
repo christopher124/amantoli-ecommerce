c275f45478eb33de0cab061fdd588113c3128014
Christopher Sosa <urri_@hotmail.com>
2022-10-19 11:21:38 -0500

    Cambios en sistema de nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,6 +12,7 @@ export class NavComponent implements OnInit {
   public id;
   public user: any = undefined;
   public user_lc: any = undefined;
+  public config_global: any = {};
 
   constructor(
     private _clienteService: ClienteService,
@@ -19,6 +20,12 @@ export class NavComponent implements OnInit {
   ) {
     this.token = localStorage.getItem('token');
     this.id = localStorage.getItem('_id');
+    this._clienteService.obtener_config_public().subscribe(
+      (res) => {
+        this.config_global = res.data;
+      },
+      (err) => {}
+    );
 
     if (this.token) {
       this._clienteService.obtener_cliente_guest(this.id, this.token).subscribe(
