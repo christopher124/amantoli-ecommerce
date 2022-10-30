83fabd60a67ea87f56f00369d52921033a350178
Christopher Sosa <urri_@hotmail.com>
2022-10-30 15:27:43 -0600

    Servicio y cambios en index

---
+++ src/app/services/guest.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { GLOBAL } from './GLOBAL';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class GuestService {
+  public url: any;
+
+  constructor(private _http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  obtener_productos_slug_publico(slug: any): Observable<any> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.get(this.url + 'obtener_productos_slug_publico/' + slug, {
+      headers: headers,
+    });
+  }
+
+  listar_productos_recomendados_publico(categoria: any): Observable<any> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.get(
+      this.url + 'listar_productos_recomendados_publico/' + categoria,
+      {
+        headers: headers,
+      }
+    );
+  }
+}
