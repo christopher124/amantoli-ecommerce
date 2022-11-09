c04e046f507ea0cfadc7421d2ad2849ddef33bcf
Christopher Sosa <urri_@hotmail.com>
2022-11-08 22:06:42 -0600

    Cambios en la api para mostrar productos

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -86,4 +86,40 @@ export class ClienteService {
       headers: headers,
     });
   }
+  agregar_carrito_cliente(data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.post(this.url + 'agregar_carrito_cliente', data, {
+      headers: headers,
+    });
+  }
+  obtener_carrito_cliente(id: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.get(this.url + 'obtener_carrito_cliente/' + id, {
+      headers: headers,
+    });
+  }
+  eliminar_carrito_cliente(id: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.delete(this.url + 'eliminar_carrito_cliente/' + id, {
+      headers: headers,
+    });
+  }
+  registro_direccion_cliente(data: any, token: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.post(this.url + 'registro_direccion_cliente', data, {
+      headers: headers,
+    });
+  }
 }
